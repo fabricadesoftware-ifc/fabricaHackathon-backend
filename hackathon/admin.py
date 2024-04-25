47a99f41bcc543040ae5cf69ca57776d8c56a7be
Hiansdt <vinijao123690@gmail.com>
2024-04-25 12:14:41 -0300

    CHORE(Admin): Change admin model imports

--- hackathon/admin.py
+++ hackathon/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Curso, Turma, Edicao, Aluno, Equipe, Avaliador, Criterio, Edicao_criterio, Avaliacao, Avaliador_edicao
+from .models import Curso, Turma, Edicao, Aluno, Equipe, Avaliador, Criterio, EdicaoCriterio, Avaliacao, AvaliadorEdicao
 
 admin.site.register(Curso)
 admin.site.register(Turma)
@@ -9,6 +9,6 @@ admin.site.register(Aluno)
 admin.site.register(Equipe)
 admin.site.register(Avaliador)
 admin.site.register(Criterio)
-admin.site.register(Edicao_criterio)
+admin.site.register(EdicaoCriterio)
 admin.site.register(Avaliacao)
-admin.site.register(Avaliador_edicao)
+admin.site.register(AvaliadorEdicao)

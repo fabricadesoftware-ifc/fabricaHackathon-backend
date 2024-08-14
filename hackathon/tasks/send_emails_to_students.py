71767c2ff35a5bb91a11f773ef4f9dbe566abcad
Hiansdt <vnhiansdt@gmail.com>
2024-08-14 13:53:57 -0300

    REFACTOR(editions): Alter e-mail function to only send to courses specific to the edition being altered [Refs: #14]

--- hackathon/tasks/send_emails_to_students.py
+++ hackathon/tasks/send_emails_to_students.py
@@ -5,10 +5,14 @@ from hackathon.models import Student, Edition
 @shared_task
 def send_emails_to_students(edition_id):
     edition = Edition.objects.get(id=edition_id)
-    student_emails = list(Student.objects.values_list('email', flat=True))
-    courses = getCourses(edition.courses.all())
-    subject = f"Inscrições abertas para o Hackathon {edition.year} ({courses})"
-    message = f"As inscrições para o Hackathon {edition.year} estão abertas, inscreva sua equipe! \n Cursos: \n {courses}"
+    courses = edition.courses.all()
+    student_emails = list(
+        Student.objects.filter(classInfo__id__in=edition.involved_classes.all()).values_list('email', flat=True)
+    )
+    subject = f"Inscrições abertas para o Hackathon {edition.year} ({getCourses(courses, ', ')})"
+    message = f"As inscrições para o Hackathon {edition.year} estão abertas, inscreva sua equipe! \n Cursos: \n {getCourses(courses, '\n')}"
+
+    print(student_emails)
 
     send_mail(
         subject,
@@ -18,6 +22,6 @@ def send_emails_to_students(edition_id):
         fail_silently=False,
     )
 
-def getCourses(edition_courses):
-    resulting_string = "\n".join(course.name for course in edition_courses)
+def getCourses(edition_courses, join_string):
+    resulting_string = join_string.join(course.name for course in edition_courses)
     return resulting_string

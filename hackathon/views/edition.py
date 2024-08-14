743e6e987dd1eff0e6615e59c38e9e61834f70bd
Hiansdt <vnhiansdt@gmail.com>
2024-08-14 08:50:59 -0300

    CHORE(editions): Rewrite update method to include applications_accepted signal [Refs: #14]

--- hackathon/views/edition.py
+++ hackathon/views/edition.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.response import Response
+from rest_framework import status
 from hackathon.models import Edition
 from hackathon.serializers import EditionWriteSerializer, EditionListSerializer, EditionRetrieveSerializer
+from hackathon.signals import applications_accepted_changed
 
 class EditionViewSet(ModelViewSet):
     queryset = Edition.objects.all()
@@ -12,3 +14,24 @@ class EditionViewSet(ModelViewSet):
         if self.action == 'retrieve':
             return EditionRetrieveSerializer
         return EditionWriteSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        old_value = instance.applications_accepted
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        new_value = instance.applications_accepted
+
+        if new_value != old_value:
+            applications_accepted_changed.send(
+                sender=Edition,
+                instance=instance,
+                old_value=old_value,
+                new_value=new_value
+            )
+
+        return Response(serializer.data)

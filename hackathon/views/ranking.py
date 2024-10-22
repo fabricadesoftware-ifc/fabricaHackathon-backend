2c1085793e2a4c925f0e2a914e735c06ea4cc57a
Hiansdt <vnhiansdt@gmail.com>
2024-10-22 18:32:07 -0300

    FEAT(Ranking): Alter Ranking view to only use detailed serializer on retrieve operations

--- hackathon/views/ranking.py
+++ hackathon/views/ranking.py
@@ -2,11 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 
 from hackathon.models import Ranking
 from hackathon.serializers import RankingDetailSerializer, RankingSerializer
+from hackathon.filters import RankingFilter
 
 class RankingViewSet(ModelViewSet):
     queryset = Ranking.objects.all()
+    filterset_class=RankingFilter
 
     def get_serializer_class(self):
-        if self.action in ['list', 'retrieve']:
+        if self.action == "retrieve":
             return RankingDetailSerializer
         return RankingSerializer
